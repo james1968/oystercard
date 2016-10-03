9b11f10ed82de5b913ee8f7a2bbc57e6b91d2bda
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-03 15:50:16 +0100

    Added a maximum balance on the card.

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -2,12 +2,14 @@ class OysterCard
 
   attr_reader :balance
 
-  def initialize
-    @balance = 0
+MAXIMUM_BALANCE = 90
+
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def top_up(integer)
+    fail "Exceeded maximum balance of £#{MAXIMUM_BALANCE}" if @balance + integer > MAXIMUM_BALANCE
     @balance += integer
   end
-
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -12,8 +12,15 @@ require 'spec_helper'
         expect(subject.balance).to eq(10)
       end
 
-      
+      it "only allows up to a maximum initial balance on the card" do
+        maximum = OysterCard::MAXIMUM_BALANCE
+        expect{subject.top_up(maximum + 1)}.to raise_error "Exceeded maximum balance of £#{maximum}"
+      end
+
+      it "can't top up more than the maximum balance" do
+        maximum = OysterCard::MAXIMUM_BALANCE
+        subject.top_up(maximum)
+        expect{subject.top_up(10)}.to raise_error "Exceeded maximum balance of £#{maximum}"
+      end
     end
-
-
   end
